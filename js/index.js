3f46b2d7d1c2cf2295070477eb154b778fdc3799
yvolaco <yvolaco@users.noreply.github.com>
2017-07-30 12:14:33 -0700

    first commit!

---
+++ js/index.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  $("#bio").click(function() {
+    $(".bio").toggleClass("selected");
+  });
+
+  $("#portfolio").click(function() {
+    $(".portfolio").toggleClass("selected");
+  });
+
+  $("#resume").click(function() {
+    $(".resume").toggleClass("selected");
+  });
+
+  $("#upcoming").click(function() {
+    $(".upcoming").toggleClass("selected");
+  });
+
+  $(".close").click(function() {
+    $(".selected").removeClass("selected");
+  });
+
+
+  $("#performancelink").click(function(){
+    $(".choreography").hide();
+    $(".performance").fadeIn(500);
+});
+
+$("#choreolink").click(function(){
+  $(".performance").hide();
+  $(".choreography").fadeIn(500);
+});
+
+
+
+  $(document).keydown(function(e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+      $(".selected").removeClass("selected");
+    }
+  });
+
+  $("#performance").unitegallery({
+    tile_enable_image_effect: true,
+    tile_enable_overlay: false,
+    tile_enable_icons: false,
+    tiles_type: "justified",
+    tiles_justified_row_height: 210,
+    tiles_justified_space_between: 10,
+    lightbox_type: "compact",
+    tile_enable_textpanel: true,
+    tile_textpanel_title_text_align: "right",
+    tile_textpanel_source: "desc",
+    lightbox_slider_image_border_width: 0,
+    lightbox_slider_image_shadow: false,
+  });
+
+  $("#choreography").unitegallery({
+    tile_enable_image_effect: true,
+    tile_enable_overlay: false,
+    tile_enable_icons: false,
+    tiles_type: "justified",
+    tiles_justified_row_height: 210,
+    tiles_justified_space_between: 10,
+    lightbox_type: "compact",
+    tile_enable_textpanel: true,
+    tile_textpanel_title_text_align: "right",
+    tile_textpanel_source: "desc",
+    lightbox_slider_image_border_width: 0,
+    lightbox_slider_image_shadow: false,
+  });
+
+});
