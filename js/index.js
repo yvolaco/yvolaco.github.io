8462d55903f0e2d153c49f6d13c4ce3123d2ec08
yvolaco <yvolaco@users.noreply.github.com>
2018-03-17 12:02:58 -0400

    edit smooth scroll function

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ $("#choreolink").click(function(){
   });
 
 
-$(function() {
+
     $('a[href*=#]:not([href=#])').click(function() {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
@@ -55,7 +55,7 @@ $(function() {
             return false;
         }
     });
-});
+
 
 
   $("#performance").unitegallery({
