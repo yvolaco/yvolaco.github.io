ffedd4c4fa92ff3a9477103e9b55a3cc0f77c83b
yvolaco <yvolaco@users.noreply.github.com>
2018-03-17 12:02:00 -0400

    add smooth scroll function

--- js/index.js
+++ js/index.js
@@ -43,6 +43,21 @@ $("#choreolink").click(function(){
     }
   });
 
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
+
+
   $("#performance").unitegallery({
     tile_enable_image_effect: true,
     tile_enable_overlay: false,
